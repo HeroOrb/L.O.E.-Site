8e1eae0470b05ea73b9b378290d556d64a88d838
HeroOrb <Connorbagnell@gmail.com>
2025-11-28 03:49:50 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let loginButton = document.getElementById('loginButton');
+
+    loginButton.addEventListener('click', function() {
+        alert('Button Has Been Pressed!');
+        window.location.href = 'dashboard.html';
+    })
+})
